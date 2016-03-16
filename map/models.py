12ccd92273b83298dd1b21f6512f85dc45bf04ee
StephenChan <StephenChan@users.noreply.github.com>
2016-03-16 00:58:51 -0700

    Add models.py to /map app so that running tests for /map doesn't result in an ImproperlyConfigured error. (Our manage.py mytest command runs tests for every app, triggering this error.)

---
+++ map/models.py
@@ -0,0 +1,4 @@
+# All of our apps must have a models.py so that our
+# "run tests for every app" command doesn't get this kind of error:
+# "ImproperlyConfigured: App with label map could not be found"
+# (As of Django 1.3.0)

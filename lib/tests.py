b4f39f634d409b11e1327e21681e1d5a0391a9cf
StephenChan <StephenChan@users.noreply.github.com>
2016-03-16 01:03:04 -0700

    Fix Contact page tests. Now have 7 working tests out of 7 here.

--- lib/tests.py
+++ lib/tests.py
@@ -24,7 +24,17 @@ class ContactTest(ClientTest):
     """
     fixtures = ['test_users.yaml']
 
-    def test_contact_success(self):
+    def test_access_page(self):
+        """
+        Access the page without errors.
+        """
+        response = self.client.get(reverse('contact'))
+        self.assertStatusOK(response)
+
+    def test_send_email_as_user(self):
+        """
+        Logged-in user successfully sends a contact email.
+        """
         username = 'user2'
         user_email = User.objects.get(username='user2').email
         self.client.login(username=username, password='secret')
@@ -36,16 +46,15 @@ class ContactTest(ClientTest):
         # Submit the form
         subject = "Subject goes here"
         message = "Message\ngoes here."
-        response = self.client.post(reverse('contact'), dict(
-            subject=subject,
-            message=message,
-        ))
-        self.assertStatusOK(response)
-        # Check that we got the expected top-of-page message.
-        self.assertMessages(
-            response,
-            [msg_consts.CONTACT_EMAIL_SENT],
+        response = self.client.post(
+            reverse('contact'),
+            dict(subject=subject, message=message),
         )
+
+        # Check that we get redirected to the index page now.
+        # This WILL redirect to another page. We won't take further steps to
+        # test which page that is. That's the index logic, not the contact logic.
+        self.assertRedirects(response, reverse('index'), target_status_code=302)
 
         # Check that 1 email was sent.
         self.assertEqual(len(mail.outbox), 1)
@@ -82,16 +91,96 @@ class ContactTest(ClientTest):
         if settings.UNIT_TEST_VERBOSITY >= 1:
             print "Email message:\n{message}".format(message=contact_email.body)
 
+    def test_send_email_as_guest(self):
+        """
+        Non-logged-in user successfully sends a contact email.
+        """
+        # Reach the page
+        response = self.client.get(reverse('contact'))
+        self.assertStatusOK(response)
+
+        # Submit the form
+        subject = "Subject goes here"
+        message = "Message\ngoes here."
+        email = "email@goeshere.com"
+        response = self.client.post(
+            reverse('contact'),
+            dict(subject=subject, message=message, email=email),
+        )
+
+        # Check that we get redirected to the index page now.
+        self.assertRedirects(response, reverse('index'), target_status_code=200)
+
+        # Check that 1 email was sent.
+        self.assertEqual(len(mail.outbox), 1)
+        contact_email = mail.outbox[0]
+
+        # Check that the email's "to" field is equal to settings.ADMINS.
+        self.assertEqual(len(contact_email.to), len(settings.ADMINS))
+        for admin_name, admin_email in settings.ADMINS:
+            self.assertIn(admin_email, contact_email.to)
+
+        # Check the subject.
+        self.assertEqual(
+            contact_email.subject,
+            "{prefix}{unprefixed_subject}".format(
+                prefix=settings.EMAIL_SUBJECT_PREFIX,
+                unprefixed_subject=str_consts.CONTACT_EMAIL_SUBJECT_FMTSTR.format(
+                    username="[A guest]",
+                    base_subject=subject,
+                )
+            ),
+        )
+        if settings.UNIT_TEST_VERBOSITY >= 1:
+            print "Email subject:\n{subject}".format(subject=contact_email.subject)
+
+        # Check the message/body.
+        self.assertEqual(
+            contact_email.body,
+            str_consts.CONTACT_EMAIL_MESSAGE_FMTSTR.format(
+                username="[A guest]",
+                user_email=email,
+                base_message=message,
+            ),
+        )
+        if settings.UNIT_TEST_VERBOSITY >= 1:
+            print "Email message:\n{message}".format(message=contact_email.body)
+
+    def test_success_message(self):
+        """
+        After sending the form, check for a top-of-page message indicating success.
+        """
+        # Reach the page
+        response = self.client.get(reverse('contact'))
+        self.assertStatusOK(response)
+
+        # Submit the form
+        subject = "Subject goes here"
+        message = "Message\ngoes here."
+        email = "email@goeshere.com"
+        response = self.client.post(
+            reverse('contact'),
+            dict(subject=subject, message=message, email=email),
+            # Ensure we get the response after the final redirect. This way we'll
+            # have access to response.context's messages.
+            follow=True,
+        )
+
+        # Check that we got the expected top-of-page message.
+        self.assertMessages(
+            response,
+            [msg_consts.CONTACT_EMAIL_SENT],
+        )
+
     def test_contact_error_required(self):
         """
         'field is required' errors.
         """
-        self.client.login(username='user2', password='secret')
-
         # Message is missing.
         response = self.client.post(reverse('contact'), dict(
             subject="Subject goes here",
             message="",
+            email="email@goeshere.com",
         ))
         self.assertStatusOK(response)
         self.assertMessages(response, [msg_consts.FORM_ERRORS])
@@ -103,11 +192,24 @@ class ContactTest(ClientTest):
         response = self.client.post(reverse('contact'), dict(
             subject="",
             message="Message\ngoes here.",
+            email="email@goeshere.com",
         ))
         self.assertStatusOK(response)
         self.assertMessages(response, [msg_consts.FORM_ERRORS])
         self.assertFormErrors(response, 'contact_form', {
             'subject': [forms.Field.default_error_messages['required']],
+        })
+
+        # Email is missing.
+        response = self.client.post(reverse('contact'), dict(
+            subject="Subject goes here",
+            message="Message\ngoes here.",
+            email="",
+        ))
+        self.assertStatusOK(response)
+        self.assertMessages(response, [msg_consts.FORM_ERRORS])
+        self.assertFormErrors(response, 'contact_form', {
+            'email': [forms.Field.default_error_messages['required']],
         })
 
     def test_contact_error_char_limit(self):
@@ -138,7 +240,7 @@ class ContactTest(ClientTest):
         })
 
     # TODO: How to test for a BadHeaderError?
-    # Putting a newline in the subject isn't getting such an error for me.
+    # Putting a newline in the subject isn't getting such an error.
 
 #        self.client.login(username='user2', password='secret')
 #

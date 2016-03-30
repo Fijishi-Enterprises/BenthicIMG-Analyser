be67e4b0cbc9ceb9262921d055126a8f69233910
StephenChan <StephenChan@users.noreply.github.com>
2016-03-30 10:25:52 -0700

    Remove verify_exists argument from the Profile model's website URLField. That argument's functionality is no longer available in Django 1.4.

--- accounts/models.py
+++ accounts/models.py
@@ -7,12 +7,5 @@ from userena.models import User
 class Profile(UserenaLanguageBaseProfile):
    user = models.OneToOneField(User, unique = True, verbose_name = _('user'),related_name = 'my_profile')
    about_me = models.CharField(_('Name'), max_length=45, blank=True)
-   website = models.URLField(_('Website'), blank=True, verify_exists=True)
+   website = models.URLField(_('Website'), blank=True)
    location =  models.CharField(_('Location'), max_length=45, blank=True)
-  
-  #  group = models.ForeignKey(Group)
-
-#class Group(models.Model):
- #   name = models.CharField(max_length=45, blank=True)
-  #  member_permission = choices("View", "Annotate", "None") #Default permissions for GROUP MEMBERS on GROUPS images
-  #  source_permission = choices("View", "Annotate", "None") #Default permissions for EVERYONE ELSE on GROUPS images

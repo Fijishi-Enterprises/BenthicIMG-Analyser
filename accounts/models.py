4801ba67e9383b544e225dac42287df353801bfc
StephenChan <StephenChan@users.noreply.github.com>
2016-04-01 01:05:57 -0700

    "About me" profile field is now labeled as "About me" on the edit form, rather than "Name". That sure escaped detection for a while...

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from userena.models import UserenaLanguageBaseProfile
-from userena.models import User
 
 class Profile(UserenaLanguageBaseProfile):
    user = models.OneToOneField(User, unique = True, verbose_name = _('user'),related_name = 'my_profile')
-   about_me = models.CharField(_('Name'), max_length=45, blank=True)
+   about_me = models.CharField(_('About me'), max_length=45, blank=True)
    website = models.URLField(_('Website'), blank=True)
    location =  models.CharField(_('Location'), max_length=45, blank=True)

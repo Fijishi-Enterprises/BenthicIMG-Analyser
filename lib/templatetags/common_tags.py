7e491774070742b1ebee226462f5d5b6638abc57
StephenChan <StephenChan@users.noreply.github.com>
2016-04-01 14:27:09 -0700

    Make the maintenance-notice code timezone aware.

--- lib/templatetags/common_tags.py
+++ lib/templatetags/common_tags.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 from django import template
 from django.conf import settings
 from django.contrib.staticfiles import finders
-from django.utils import simplejson
+from django.utils import simplejson, timezone
 from django.utils.safestring import mark_safe
 
 register = template.Library()
@@ -75,39 +75,47 @@ class SetMaintenanceTimeNode(template.Node):
             year_day_and_time = '%Y %b %d %I:%M %p',  # 2008 Jun 24 12:34 PM
         )
         datetime_obj = None
-        now = datetime.now()
+        now = timezone.now()
 
         # Parse the input, trying each acceptable datetime format.
         # Then infer the full date and time.
         for key, datetime_format_str in datetime_format_strs.iteritems():
             try:
                 input = datetime.strptime(datetime_str, datetime_format_str)
+                input = timezone.make_aware(input, timezone.get_current_timezone())
+
                 if key == 'time':
                     # Just the hour and minute were given.
+                    naive_datetime_obj = datetime(
+                        now.year, now.month, now.day, input.hour, input.minute)
+                    datetime_obj = timezone.make_aware(
+                        naive_datetime_obj, timezone.get_current_timezone())
                     # To find whether the intended day is yesterday,
                     # today, or tomorrow, assume that the admin meant a time
                     # within 12 hours (either way) from now.
-                    datetime_obj = datetime(now.year, now.month, now.day,
-                                            input.hour, input.minute)
                     if datetime_obj - now > timedelta(0.5):
+                        # e.g. datetime_obj = 11 PM, now = 1 AM
+                        # Assume datetime_obj should be in the previous day,
+                        # meaning we went under maintenance 2 hours ago.
                         datetime_obj = datetime_obj - timedelta(1)
                     elif now - datetime_obj > timedelta(0.5):
+                        # e.g. datetime_obj = 1 AM, now = 11 PM
+                        # Assume datetime_obj should be in the next day,
+                        # meaning we plan to go under maintenance in 2 hours.
                         datetime_obj = datetime_obj + timedelta(1)
                 elif key == 'day_and_time':
                     # The month, day, hour, and minute were given.
-                    # To find whether the intended year is last year,
-                    # this year, or next year, assume that the admin meant a
-                    # date within ~half a year (either way) from now.
-                    # This just handles the corner case if you're doing
-                    # maintenance instead of celebrating the new year...
-                    datetime_obj = datetime(now.year, input.month, input.day,
+                    naive_datetime_obj = datetime(
+                        now.year, input.month, input.day,
                         input.hour, input.minute)
-                    if datetime_obj - now > timedelta(182.5):
-                        datetime_obj = datetime(now.year-1, input.month, input.day,
-                            input.hour, input.minute)
-                    elif now - datetime_obj > timedelta(182.5):
-                        datetime_obj = datetime(now.year+1, input.month, input.day,
-                            input.hour, input.minute)
+                    datetime_obj = timezone.make_aware(
+                        naive_datetime_obj, timezone.get_current_timezone())
+                    # Unlike the 'time' case, we won't bother dealing with
+                    # the 'maintenance next year' or 'maintenance last year'
+                    # corner case here. Adding/subtracting 1 year is sketchy
+                    # due to leap years.
+                    # Just be sure to use the 'year_day_and_time' case if it's
+                    # close to the new year.
                 elif key == 'year_day_and_time':
                     # The full date and time were given.
                     datetime_obj = input
@@ -129,9 +137,10 @@ def set_maintenance_time(parser, token):
 #
 # Prevent undesired browser caching of static files (CSS, JS, etc.)
 # by adding a version string after the filename in the link/script element.
-# The version string is the last-modified time of the file, which means
-# the string changes (and thus, the browser re-fetches) if and only if
-# the file has been modified.
+# The version string is the last-modified time of the file, as a timezone
+# agnostic Unix timestamp.
+# So the version string changes (and thus, the browser re-fetches)
+# if and only if the file has been modified.
 #
 # Usage: {% versioned_static "js/util.js" %}
 # Example output: {{ STATIC_URL }}js/util.js?version=1035720937

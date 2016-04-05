6858704051a3f102fed678790ad2f1aaa7480bef
StephenChan <StephenChan@users.noreply.github.com>
2016-04-04 22:27:41 -0700

    Remove LOGGING setting value for now, because we're just matching the default for now.

--- config/settings/base.py
+++ config/settings/base.py
@@ -236,33 +236,6 @@ LOGIN_URL = '/accounts/signin/'
 LOGOUT_URL = '/accounts/signout/'
 LOGIN_REDIRECT_URL = '/images/source/'
 
-# Logging configuration.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-
 # The name of the class to use to run the test suite
 TEST_RUNNER = 'lib.test_utils.MyTestSuiteRunner'
 

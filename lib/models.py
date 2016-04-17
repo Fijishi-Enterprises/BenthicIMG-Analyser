2fe19f41b26379cf4b782ba0391c729dd81dd6a7
StephenChan <StephenChan@users.noreply.github.com>
2016-04-16 17:02:52 -0700

    Remove empty models.py files; as of Django 1.7 they're no longer needed for app recognition.

--- lib/models.py
+++
@@ -1,3 +0,0 @@
-# Even if this app doesn't need models, we still need
-# a models.py so that Django officially recognizes this app.
-# For example, it's needed to run any tests for this app.
--- map/models.py
+++
@@ -1,4 +0,0 @@
-# All of our apps must have a models.py so that our
-# "run tests for every app" command doesn't get this kind of error:
-# "ImproperlyConfigured: App with label map could not be found"
-# (As of Django 1.3.0)

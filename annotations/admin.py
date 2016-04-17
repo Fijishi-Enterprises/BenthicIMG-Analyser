27cb2ab17ed7ab61cba0b5b8ce9e707e50ffc1cf
StephenChan <StephenChan@users.noreply.github.com>
2016-04-16 22:12:15 -0700

    Use new admin.register decorator instead of admin.site.register function, for slightly cleaner looking code.

--- annotations/admin.py
+++ annotations/admin.py
@@ -2,20 +2,19 @@ from annotations.models import Label, LabelSet, LabelGroup, Annotation
 from django.contrib import admin
 from reversion.admin import VersionAdmin
 
+@admin.register(Label)
 class LabelAdmin(admin.ModelAdmin):
     list_display = ('name', 'code', 'group', 'create_date')
 
+@admin.register(LabelSet)
 class LabelSetAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(LabelGroup)
 class LabelGroupAdmin(admin.ModelAdmin):
     list_display = ('name', 'code')
 
 # Inherit from reversion.VersionAdmin to enable versioning for a particular model.
+@admin.register(Annotation)
 class AnnotationAdmin(VersionAdmin):
     list_display = ('source', 'image', 'point')
-
-admin.site.register(Label, LabelAdmin)
-admin.site.register(LabelSet, LabelSetAdmin)
-admin.site.register(LabelGroup, LabelGroupAdmin)
-admin.site.register(Annotation, AnnotationAdmin)
--- images/admin.py
+++ images/admin.py
@@ -2,15 +2,14 @@ from images.models import Source, Image, Metadata
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
 
+@admin.register(Source)
 class SourceAdmin(GuardedModelAdmin):
     list_display = ('name', 'visibility', 'create_date')
 
+@admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('original_file', 'source', 'metadata')
 
+@admin.register(Metadata)
 class MetadataAdmin(admin.ModelAdmin):
     list_display = ('name', 'value1', 'value2', 'value3', 'value4', 'value5')
-
-admin.site.register(Source, SourceAdmin)
-admin.site.register(Image, ImageAdmin)
-admin.site.register(Metadata, MetadataAdmin)

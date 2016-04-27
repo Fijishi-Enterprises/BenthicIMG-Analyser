da8a462e1ddb78a23fb0af24131319ffac23e710
StephenChan <StephenChan@users.noreply.github.com>
2016-04-26 23:56:24 -0700

    Simplify template tag code, now that @simple_tag supports the 'as' clause out of the box (as of Django 1.9).

--- lib/templatetags/common_tags.py
+++ lib/templatetags/common_tags.py
@@ -1,11 +1,8 @@
-# Custom template filters/tags need to live inside an app
-# (http://www.b-list.org/weblog/2006/sep/10/django-tips-laying-out-application/),
-# but some filters/tags are general and don't really belong in a specific
-# app.  This file is for those general filters/tags.
+# General-use custom template tags and filters.
+
 
 import json
 import os.path
-import re
 from datetime import datetime, timedelta
 from django import template
 from django.conf import settings
@@ -16,32 +13,10 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 
-def do_assignment_tag(parser, token, cls):
-    # Uses a regular expression to parse tag contents.
-    try:
-        # Splitting by None == splitting by spaces.
-        tag_name, arg = token.contents.split(None, 1)
-    except ValueError:
-        raise template.TemplateSyntaxError("%r tag requires arguments" % token.contents.split()[0])
-    m = re.search(r'(.*?) as (\w+)', arg)
-    if not m:
-        raise template.TemplateSyntaxError("%r tag had invalid arguments" % tag_name)
-    arg1, result_var_name = m.groups()
-    return cls(arg1, result_var_name)
-
-
 # Usage: {% get_form_media form as form_media %}
-class GetFormMediaNode(template.Node):
-    def __init__(self, form_context_var, result_var_name):
-        self.form_context_var = template.Variable(form_context_var)
-        self.result_var_name = result_var_name
-    def render(self, context):
-        form = self.form_context_var.resolve(context)
-        context[self.result_var_name] = dict(js=form.media._js, css=form.media._css)
-        return ''
-@register.tag
-def get_form_media(parser, token):
-    return do_assignment_tag(parser, token, GetFormMediaNode)
+@register.simple_tag
+def get_form_media(form):
+    return dict(js=form.media._js, css=form.media._css)
 
 
 # jsonify
@@ -60,77 +35,68 @@ def jsonify(object):
 
 
 # Usage: {% set_maintenance_time "9:00 PM" as maintenance_time %}
-class SetMaintenanceTimeNode(template.Node):
-    def __init__(self, context_var, result_var_name):
-        self.context_var = template.Variable(context_var)
-        self.result_var_name = result_var_name
+@register.simple_tag
+def set_maintenance_time(datetime_str):
 
-    def render(self, context):
-        datetime_str = self.context_var.resolve(context)
+    # Acceptable datetime formats.
+    datetime_format_strs = dict(
+        time = '%I:%M %p',                        # 12:34 PM
+        day_and_time = '%b %d %I:%M %p',          # Jun 24 12:34 PM
+        year_day_and_time = '%Y %b %d %I:%M %p',  # 2008 Jun 24 12:34 PM
+    )
+    datetime_obj = None
+    now = timezone.now()
 
-        # Acceptable datetime formats.
-        datetime_format_strs = dict(
-            time = '%I:%M %p',                        # 12:34 PM
-            day_and_time = '%b %d %I:%M %p',          # Jun 24 12:34 PM
-            year_day_and_time = '%Y %b %d %I:%M %p',  # 2008 Jun 24 12:34 PM
-        )
-        datetime_obj = None
-        now = timezone.now()
+    # Parse the input, trying each acceptable datetime format.
+    # Then infer the full date and time.
+    for key, datetime_format_str in datetime_format_strs.iteritems():
+        try:
+            input = datetime.strptime(datetime_str, datetime_format_str)
+            input = timezone.make_aware(input, timezone.get_current_timezone())
 
-        # Parse the input, trying each acceptable datetime format.
-        # Then infer the full date and time.
-        for key, datetime_format_str in datetime_format_strs.iteritems():
-            try:
-                input = datetime.strptime(datetime_str, datetime_format_str)
-                input = timezone.make_aware(input, timezone.get_current_timezone())
+            if key == 'time':
+                # Just the hour and minute were given.
+                naive_datetime_obj = datetime(
+                    now.year, now.month, now.day, input.hour, input.minute)
+                datetime_obj = timezone.make_aware(
+                    naive_datetime_obj, timezone.get_current_timezone())
+                # To find whether the intended day is yesterday,
+                # today, or tomorrow, assume that the admin meant a time
+                # within 12 hours (either way) from now.
+                if datetime_obj - now > timedelta(0.5):
+                    # e.g. datetime_obj = 11 PM, now = 1 AM
+                    # Assume datetime_obj should be in the previous day,
+                    # meaning we went under maintenance 2 hours ago.
+                    datetime_obj = datetime_obj - timedelta(1)
+                elif now - datetime_obj > timedelta(0.5):
+                    # e.g. datetime_obj = 1 AM, now = 11 PM
+                    # Assume datetime_obj should be in the next day,
+                    # meaning we plan to go under maintenance in 2 hours.
+                    datetime_obj = datetime_obj + timedelta(1)
+            elif key == 'day_and_time':
+                # The month, day, hour, and minute were given.
+                naive_datetime_obj = datetime(
+                    now.year, input.month, input.day,
+                    input.hour, input.minute)
+                datetime_obj = timezone.make_aware(
+                    naive_datetime_obj, timezone.get_current_timezone())
+                # Unlike the 'time' case, we won't bother dealing with
+                # the 'maintenance next year' or 'maintenance last year'
+                # corner case here. Adding/subtracting 1 year is sketchy
+                # due to leap years.
+                # Just be sure to use the 'year_day_and_time' case if it's
+                # close to the new year.
+            elif key == 'year_day_and_time':
+                # The full date and time were given.
+                datetime_obj = input
 
-                if key == 'time':
-                    # Just the hour and minute were given.
-                    naive_datetime_obj = datetime(
-                        now.year, now.month, now.day, input.hour, input.minute)
-                    datetime_obj = timezone.make_aware(
-                        naive_datetime_obj, timezone.get_current_timezone())
-                    # To find whether the intended day is yesterday,
-                    # today, or tomorrow, assume that the admin meant a time
-                    # within 12 hours (either way) from now.
-                    if datetime_obj - now > timedelta(0.5):
-                        # e.g. datetime_obj = 11 PM, now = 1 AM
-                        # Assume datetime_obj should be in the previous day,
-                        # meaning we went under maintenance 2 hours ago.
-                        datetime_obj = datetime_obj - timedelta(1)
-                    elif now - datetime_obj > timedelta(0.5):
-                        # e.g. datetime_obj = 1 AM, now = 11 PM
-                        # Assume datetime_obj should be in the next day,
-                        # meaning we plan to go under maintenance in 2 hours.
-                        datetime_obj = datetime_obj + timedelta(1)
-                elif key == 'day_and_time':
-                    # The month, day, hour, and minute were given.
-                    naive_datetime_obj = datetime(
-                        now.year, input.month, input.day,
-                        input.hour, input.minute)
-                    datetime_obj = timezone.make_aware(
-                        naive_datetime_obj, timezone.get_current_timezone())
-                    # Unlike the 'time' case, we won't bother dealing with
-                    # the 'maintenance next year' or 'maintenance last year'
-                    # corner case here. Adding/subtracting 1 year is sketchy
-                    # due to leap years.
-                    # Just be sure to use the 'year_day_and_time' case if it's
-                    # close to the new year.
-                elif key == 'year_day_and_time':
-                    # The full date and time were given.
-                    datetime_obj = input
+            # We've got the date and time, so we're done.
+            break
 
-                # We've got the date and time, so we're done.
-                break
+        except ValueError:
+            continue
 
-            except ValueError:
-                continue
-
-        context[self.result_var_name] = datetime_obj
-        return ''
-@register.tag
-def set_maintenance_time(parser, token):
-    return do_assignment_tag(parser, token, SetMaintenanceTimeNode)
+    return datetime_obj
 
 
 # versioned_static

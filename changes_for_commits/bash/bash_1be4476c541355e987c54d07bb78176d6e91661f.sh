f77bb4fbc3b1dd4065b1c75ba80aa370903ed890
StephenChan <StephenChan@users.noreply.github.com>
2016-04-29 19:14:26 -0700

    Remove changes_for_commits. The contents are really old, and we never ended up using these bash scripts anyway. If we feel a need for a similar idea in the future, we should also indicate the date of each change, not just the commit hash.

--- changes_for_commits/bash/bash_1be4476c541355e987c54d07bb78176d6e91661f.sh
+++
@@ -1,5 +0,0 @@
-# Migrate to get the new bug_reporting models.
-# Can also do "./manage.py migrate bug_reporting"
-# so that South doesn't even check the other apps.
-
-./manage.py migrate
--- changes_for_commits/bash/bash_3749eddf33ad6bc92e523cc9eda943823aff46b5.sh
+++
@@ -1,4 +0,0 @@
-# Install dajaxice and dajax, for easier Ajax coding.
-
-easy_install django-dajaxice
-easy_install django-dajax

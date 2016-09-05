beb69fb0a3c0f05a8e61285011bb88f9025c61b7
StephenChan <StephenChan@users.noreply.github.com>
2016-09-05 15:03:15 -0700

    Remove unused imports in visualization/views.py.

--- project/visualization/views.py
+++ project/visualization/views.py
@@ -1,13 +1,9 @@
-import csv
-from django.forms import modelformset_factory
-
-from numpy import zeros, sum, linalg, logical_and, vectorize
-
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.db import transaction
+from django.forms import modelformset_factory
 from django.forms.formsets import formset_factory
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 
 from .forms import CheckboxForm, StatisticsSearchForm, ImageSearchForm, \
@@ -18,7 +14,7 @@ from annotations.models import Annotation, Label, LabelSet, LabelGroup
 from images.forms import MetadataFormForGrid, BaseMetadataFormSet
 from images.models import Source, Image, Metadata
 from images.tasks import *
-from images.utils import delete_images, get_aux_metadata_str_list_for_image, get_aux_labels
+from images.utils import delete_images
 from lib.decorators import source_visibility_required, source_permission_required
 
 

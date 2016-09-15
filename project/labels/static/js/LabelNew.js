a5f9fe1842d29c7ba4b33f960237acfd8a108d70
StephenChan <StephenChan@users.noreply.github.com>
2016-09-15 12:15:43 -0700

    Prevent double submission of new label form. After new label form creates a label successfully, clear the fields to facilitate creating another label.

--- project/labels/static/js/LabelNew.js
+++ project/labels/static/js/LabelNew.js
@@ -4,6 +4,7 @@
 var LabelNew = (function() {
 
     var afterLabelCreated = null;
+    var formSubmitEnabled = null;
 
     var $formStatus = null;
     var $form = null;
@@ -29,11 +30,15 @@ var LabelNew = (function() {
             success: handleSubmitResponse,
             error: util.handleServerError
         });
+
+        $formStatus.text("Submitting...");
+        formSubmitEnabled = false;
     }
 
 
     function handleSubmitResponse(response) {
         $formStatus.empty();
+        formSubmitEnabled = true;
 
         if (response['error']) {
             // Response is JSON which contains the error
@@ -43,9 +48,12 @@ var LabelNew = (function() {
         }
 
         // HTML response with button/dialog elements for the new label
-        $formStatus.text("Label successfully created.");
         afterLabelCreated(response);
-        // TODO: Clear the form?
+
+        $formStatus.text("Label successfully created.");
+
+        // Clear the form fields in case they want to create another label
+        $form.find('input:text, input:file, select, textarea').val('');
     }
 
 
@@ -60,16 +68,20 @@ var LabelNew = (function() {
 
             $formStatus = $('#label-form-status');
 
+            formSubmitEnabled = true;
+
             $form = $('#label-form');
             $form.submit(function() {
-                try {
-                    submitForm();
-                }
-                catch (e) {
-                    // Don't crash on an error so that we can
-                    // return from this function as planned.
-                    // This makes error logging a bit less nice though.
-                    console.log(e);
+                if (formSubmitEnabled) {
+                    try {
+                        submitForm();
+                    }
+                    catch (e) {
+                        // Don't crash on an error so that we can
+                        // return from this function as planned.
+                        // This makes error logging a bit less nice though.
+                        console.log(e);
+                    }
                 }
                 // Don't let the form do a non-Ajax submit.
                 return false;

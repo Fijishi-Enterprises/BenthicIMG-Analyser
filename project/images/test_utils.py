aa568d580e8bdd94f3f566e1cf85a174d552b2ff
Oscar Beijbom <oscar.beijbom@gmail.com>
2016-09-15 10:51:17 -0700

    helper for tests. Simple right now, but can perhaps be used to create models both in automated and manual testing

---
+++ project/images/test_utils.py
@@ -0,0 +1,29 @@
+"""
+This module contains scripts for manual testing.
+"""
+
+from .models import Image, Source, ImageStatus, Metadata
+from vision_backend.models import Features
+
+
+def create_image(source_name):
+	source = Source()
+	source.name = source_name
+	source.save()
+	status = ImageStatus()
+	status.save()
+	m = Metadata()
+	m.name = 'a name goes here'
+	m.save()
+	feat = Features()
+	feat.save()
+
+	img = Image()
+	img.name = 'Oscarimage'
+	img.original_width = 10
+	img.original_height = 10
+	img.status = status
+	img.source = source
+	img.metadata = m
+	img.features = feat
+	img.save()

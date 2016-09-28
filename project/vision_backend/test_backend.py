476c11380ed12dc743a1e8bb96393f383999ad4e
Oscar Beijbom <oscar.beijbom@gmail.com>
2016-09-27 22:26:45 -0700

    added test that features are exracted automatically on upload

--- project/vision_backend/test_backend.py
+++ project/vision_backend/test_backend.py
@@ -1,4 +1,11 @@
-from lib.test_utils import ClientTest
+import time
+from unittest import skipIf
+
+from django.conf import settings
+
+from lib.test_utils import ClientTest, get_total_messages_in_jobs_queue
+from images.models import Image
+from .tasks import collect_all_jobs
 
 
 class ImageInitialStatusTest(ClientTest):
@@ -9,17 +16,45 @@ class ImageInitialStatusTest(ClientTest):
     def setUpTestData(cls):
         super(ImageInitialStatusTest, cls).setUpTestData()
 
-        cls.user = cls.create_user()
-        cls.source = cls.create_source(cls.user)
-        cls.img1 = cls.upload_image_new(cls.user, cls.source)
-
     def test_features_extracted_false(self):
+        self.user = self.create_user()
+        self.source = self.create_source(self.user)
+        self.img1 = self.upload_image_new(self.user, self.source)
         self.assertFalse(self.img1.features.extracted)
 
+@skipIf(not settings.DEFAULT_FILE_STORAGE == 'lib.storage_backends.MediaStorageS3', "Can't run backend tests locally")
+@skipIf(get_total_messages_in_jobs_queue() > 10, "Too many messages in backend queue. Skipping this test.")
+class ImageGetsFeaturesTest(ClientTest):
+    """
+    Check that features for a newly uploaded images automatically gets extracted. 
+    """
+    @classmethod
+    def setUpTestData(cls):
+        super(ImageGetsFeaturesTest, cls).setUpTestData()
+        settings.FORCE_NO_BACKEND_SUBMIT = False
+        cls.user = cls.create_user()
+        cls.source = cls.create_source(cls.user)
+        
+    def test_features_extraction(self):
+
+        self.img1 = self.upload_image_new(self.user, self.source)
+        self.assertFalse(self.img1.features.extracted)
+
+        # Loop until jobs queue is empty or until features are extracted
+        while get_total_messages_in_jobs_queue() > 0 and not self.img1.features.extracted:
+            print "z",
+            time.sleep(2)
+            collect_all_jobs()
+            self.img1 = Image.objects.get(id = self.img1.id)
+
+        self.assertTrue(self.img1.features.extracted)
 
 # TODO: Test:
 # - Successful run of each backend task
 # - For applicable tasks, a subsequent task run after more data
-#   becomes available
+#   becomes available.
 # - Classification does not overwrite confirmed annotations, including
 #   in images which have some confirmed annotations and some not confirmed
+# - Scores
+#   + Test that scores disappear when points are removed.
+#   + Test that scores are added when calling "classify".

fab138e8d560632bc8fcec4212e31c866a56dd11
Oscar Beijbom <oscar.beijbom@gmail.com>
2016-09-30 16:21:37 -0700

    added a default for FORCE_VISION_BACKEND_SUPRESSION. And comments for all settings

--- project/config/settings/vision_backend.py
+++ project/config/settings/vision_backend.py
@@ -1,12 +1,16 @@
 # VISION BACKEND SETTINGS
 # TODO: move to separate settings file.
 
+# How many more annotated images are required before we try to train a new classifier.
 NEW_CLASSIFIER_TRAIN_TH = 1.1
 
+# How much better than previous classifiers must a new one be in order to get accepted.
 NEW_CLASSIFIER_IMPROVEMENT_TH = 1.02
 
+# These many must be annotated before a first classifier is trained.
 MIN_NBR_ANNOTATED_IMAGES = 20
 
+# Naming schemes
 FEATURE_VECTOR_FILE_PATTERN = '{full_image_path}.featurevector'
 
 ROBOT_MODEL_FILE_PATTERN = '{media}/classifiers/{pk}.model'
@@ -17,6 +21,11 @@ ROBOT_MODEL_VALDATA_PATTERN = '{media}/classifiers/{pk}.valdata'
 
 ROBOT_MODEL_VALRESULT_PATTERN = '{media}/classifiers/{pk}.valresult'
 
+# This indicates the max number of scores we store per point.
 NBR_SCORES_PER_ANNOTATION = 5
 
+# This is the number of epochs we request the SGD solver to take over the data.
 NBR_TRAINING_EPOCHS = 5
+
+# This should always be false except for certain unit-test situations.
+FORCE_NO_BACKEND_SUBMIT = False

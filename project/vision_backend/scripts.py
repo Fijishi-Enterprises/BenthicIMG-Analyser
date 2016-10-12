0ae84346a5b3abee1b809f58c442e81db69012ee
Oscar Beijbom <oscar.beijbom@gmail.com>
2016-10-11 23:36:05 -0700

    updated scripts file

--- project/vision_backend/scripts.py
+++ project/vision_backend/scripts.py
@@ -1,22 +1,33 @@
 from images.models import Source, Image, Point
 from .models import Score
+from .task_helpers import _read_message
 
 def print_image_scores(image_id):
-	"""
-	Print image scores to console. For debugging purposes.
-	"""
-	points = Point.objects.filter(image_id = image_id).order_by('id')
-	for enu, point in enumerate(points):
-		print '===', enu, point.row, point.column, '==='
-		for score in Score.objects.filter(point = point):
-			print score.label, score.score
+    """
+    Print image scores to console. For debugging purposes.
+    """
+    points = Point.objects.filter(image_id = image_id).order_by('id')
+    for enu, point in enumerate(points):
+        print '===', enu, point.row, point.column, '==='
+        for score in Score.objects.filter(point = point):
+            print score.label, score.score
 
 
 def set_alleviate_to_zero():
-	"""
-	This scripts set the level of alleviation to 0 for all sources
-	"""
+    """
+    This scripts set the level of alleviation to 0 for all sources
+    """
     for source in Source.objects.filter(enable_robot_classifier=True):
         print "Processing source id:" + str(source.id)
         source.alleviate_threshold = 0
-        source.save()			
+        source.save()           
+
+def read_error_messages():
+    message = _read_message('spacer_errors')
+    while not message == None:
+        print message.get_body()
+        message.delete()
+        message = _read_message('spacer_errors')
+
+
+

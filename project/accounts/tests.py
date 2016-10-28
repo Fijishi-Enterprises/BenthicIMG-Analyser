f490617b8e7beb35d8c0d04f7a3223f76535ef05
StephenChan <StephenChan@users.noreply.github.com>
2016-10-28 13:13:40 -0700

    Unit tests for email change.

--- project/accounts/tests.py
+++ project/accounts/tests.py
@@ -1,3 +1,4 @@
+import time
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core import mail
@@ -155,6 +156,168 @@ class RegisterTest(ClientTest):
         ))
         # Should work (we should be past the login page now).
         self.assertTemplateNotUsed(response, 'registration/login.html')
+
+
+class EmailChangeTest(ClientTest):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Call the parent's setup (while still using this class as cls)
+        super(EmailChangeTest, cls).setUpTestData()
+
+        cls.user = cls.create_user(
+            username='sampleUsername', password='samplePassword',
+            email='old.email.address@example.com')
+        cls.user2 = cls.create_user()
+
+    def submit_and_get_confirmation_link(self):
+        """Shortcut function for tests focusing on the confirmation step."""
+        self.client.force_login(self.user)
+        self.client.post(reverse('email_change'), dict(
+            email='new.email.address@example.com'))
+
+        confirmation_email = mail.outbox[-2]
+        # Confirmation link: should be the first link (first "word" with '://')
+        # in the confirmation email.
+        confirmation_link = None
+        for word in confirmation_email.body.split():
+            if '://' in word:
+                confirmation_link = word
+                break
+        self.assertIsNotNone(confirmation_link)
+        return confirmation_link
+
+    def test_load_submit_page_signed_out(self):
+        """Load page while logged out -> login page."""
+        response = self.client.get(reverse('email_change'))
+        self.assertRedirects(
+            response,
+            reverse(settings.LOGIN_URL)+'?next='+reverse('email_change'),
+        )
+
+    def test_load_submit_page(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('email_change'))
+        self.assertTemplateUsed(response, 'accounts/email_change_form.html')
+
+    def test_submit(self):
+        self.client.force_login(self.user)
+        response = self.client.post(
+            reverse('email_change'),
+            dict(email='new.email.address@example.com'), follow=True)
+        self.assertTemplateUsed(response, 'accounts/email_change_done.html')
+
+    def test_submit_confirmation_email_details(self):
+        self.client.force_login(self.user)
+        self.client.post(reverse('email_change'), dict(
+            email='new.email.address@example.com'))
+
+        confirmation_email = mail.outbox[-2]
+        self.assertListEqual(
+            confirmation_email.to, ['new.email.address@example.com'])
+        self.assertIn(self.user.username, confirmation_email.body)
+        self.assertIn(
+            "{h} hours".format(h=settings.EMAIL_CHANGE_CONFIRMATION_HOURS),
+            confirmation_email.body)
+
+    def test_submit_notice_email_details(self):
+        self.client.force_login(self.user)
+        self.client.post(reverse('email_change'), dict(
+            email='new.email.address@example.com'))
+
+        notice_email = mail.outbox[-1]
+        self.assertListEqual(
+            notice_email.to, ['old.email.address@example.com'])
+        self.assertIn(self.user.username, notice_email.body)
+        self.assertIn('new.email.address@example.com', notice_email.body)
+        self.assertIn(
+            "{h} hours".format(h=settings.EMAIL_CHANGE_CONFIRMATION_HOURS),
+            notice_email.body)
+
+    def test_confirm(self):
+        confirmation_link = self.submit_and_get_confirmation_link()
+
+        # Navigate to the confirmation link.
+        response = self.client.get(confirmation_link, follow=True)
+        self.assertTemplateUsed(response, 'accounts/email_change_complete.html')
+
+        # Check that the email has changed.
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.email, 'new.email.address@example.com')
+
+    def test_submit_invalid_email(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('email_change'), dict(
+            email='not.an.email.address*AT*example.com'))
+        self.assertTemplateUsed(response, 'accounts/email_change_form.html')
+        self.assertContains(response, "Enter a valid email address.")
+
+    def test_confirm_signed_out(self):
+        confirmation_link = self.submit_and_get_confirmation_link()
+        # We'll assume the key is between the last two slashes
+        # of the confirm URL.
+        confirmation_key = confirmation_link.split('/')[-2]
+
+        # Navigate to the confirmation link while signed out.
+        # Should show sign-in page.
+        self.client.logout()
+        sign_in_url = (
+            reverse(settings.LOGIN_URL) + '?next='
+            + reverse('email_change_confirm', args=[confirmation_key])
+                .replace(':', '%3A'))
+        response = self.client.get(confirmation_link)
+        self.assertRedirects(response, sign_in_url)
+        # The email should not have changed.
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.email, 'old.email.address@example.com')
+
+        # Now sign in. Should complete the process.
+        response = self.client.post(
+            sign_in_url,
+            dict(username='sampleUsername', password='samplePassword'),
+            follow=True,
+        )
+        self.assertTemplateUsed(response, 'accounts/email_change_complete.html')
+        # The email should have changed.
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.email, 'new.email.address@example.com')
+
+    def test_confirm_invalid_key(self):
+        confirmation_link = self.submit_and_get_confirmation_link()
+
+        # Chop characters off of the end of the URL to get an invalid key.
+        # (Note that the last char is a slash, so must chop at least 2.)
+        response = self.client.get(confirmation_link[:-3], follow=True)
+        self.assertTemplateUsed(response, 'accounts/email_change_confirm.html')
+        # The email should not have changed.
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.email, 'old.email.address@example.com')
+
+    def test_confirm_expired_key(self):
+        # Have a confirmation-key expiration time of 0.5 seconds
+        with self.settings(EMAIL_CHANGE_CONFIRMATION_HOURS=(1.0 / 7200.0)):
+            confirmation_link = self.submit_and_get_confirmation_link()
+
+            # Wait 1 second before using the confirmation link
+            time.sleep(1)
+            response = self.client.get(confirmation_link, follow=True)
+            self.assertTemplateUsed(
+                response, 'accounts/email_change_confirm.html')
+
+        # The email should not have changed.
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.email, 'old.email.address@example.com')
+
+    def test_confirm_signed_in_as_other_user(self):
+        confirmation_link = self.submit_and_get_confirmation_link()
+
+        # Attempt to confirm as a different user.
+        self.client.force_login(self.user2)
+        response = self.client.get(confirmation_link)
+        self.assertTemplateUsed(response, 'accounts/email_change_confirm.html')
+        # The email should not have changed.
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.email, 'old.email.address@example.com')
 
 
 class EmailAllTest(ClientTest):

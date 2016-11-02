3a503ae4c743ca6fb5adfe5f834533aacf18831b
StephenChan <StephenChan@users.noreply.github.com>
2016-11-01 21:05:56 -0700

    Implement staging settings by mostly importing from production.

--- project/config/settings/staging.py
+++ project/config/settings/staging.py
@@ -1,116 +1,19 @@
-from .base import *
+# Settings for a test environment which is as close to production as possible.
 
-
-
-# Directory for any site related files, not just the repository.
-SITE_DIR = PROJECT_DIR.ancestor(2)
+from .production import *
 
 
 
 DEBUG = True
 
-# People who get code error notifications.
-# In the format [('Full Name', 'email@example.com'),
-# ('Full Name', 'anotheremail@example.com')]
-# TODO: Get email working in production, then uncomment this.
-ADMINS = [
-#    ('Stephen', 'stephenjchan@gmail.com'),
-#    ('Oscar', 'oscar.beijbom@gmail.com'),
-#    ('CoralNet', 'coralnet@eng.ucsd.edu'),
-]
+# E-mail address that error messages come from.
+SERVER_EMAIL = 'noreply_staging@coralnet.ucsd.edu'
 
-# Hosts/domain names that are valid for this site.
-# "*" matches anything, ".example.com" matches example.com and all subdomains
-# TODO: When server setup is more settled in, only allow one host/domain.
-ALLOWED_HOSTS = ['.ucsd.edu', '.amazonaws.com', '127.0.0.1']
+# Default email address to use for various automated correspondence
+# from the site manager(s).
+DEFAULT_FROM_EMAIL = SERVER_EMAIL
 
-# Local time zone for this installation. All choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name (although not all
-# systems may support all possibilities). When USE_TZ is True, this is
-# interpreted as the default user time zone.
-TIME_ZONE = 'America/Los_Angeles'
-
-# Not-necessarily-technical managers of the site. They get broken link
-# notifications and other various emails.
-MANAGERS = ADMINS
-
-# django-storages settings
-# http://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
-AWS_ACCESS_KEY_ID = get_secret('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = get_secret('AWS_STORAGE_BUCKET_NAME')
-# Default ACL permissions when saving S3 files
-AWS_DEFAULT_ACL = 'private'
-
-# Default file storage mechanism that holds media.
-DEFAULT_FILE_STORAGE = 'lib.storage_backends.MediaStorageS3'
-
-# easy_thumbnails 3rd party app:
-# Default file storage for saving generated thumbnails.
-#
-# The only downside of not using the app's provided storage class is that
-# the THUMBNAIL_MEDIA_ROOT and THUMBNAIL_MEDIA_URL settings won't work
-# (we'd have to apply them manually). We aren't using these settings, though.
-THUMBNAIL_DEFAULT_STORAGE = DEFAULT_FILE_STORAGE
-
-# [Custom settings]
-# S3 details on storing media.
-AWS_S3_DOMAIN = 's3-us-west-2.amazonaws.com/{bucket_name}'.format(
-    bucket_name=AWS_STORAGE_BUCKET_NAME)
-AWS_S3_MEDIA_SUBDIR = 'media'
-
-# Base URL for user-uploaded media.
-# Example: "http://media.lawrence.com/media/"
-MEDIA_URL = 'https://{domain}/{subdir}/'.format(
-    domain=AWS_S3_DOMAIN, subdir=AWS_S3_MEDIA_SUBDIR)
-
-# [django-storages setting]
-# S3 bucket subdirectory in which to store media.
-AWS_LOCATION = AWS_S3_MEDIA_SUBDIR
-
-# Temporary setting to make the old processing code exception-free for now...
-PROCESSING_ROOT = SITE_DIR.child('processing')
-
-# Absolute path to the directory which static files should be collected to.
-# Example: "/home/media/media.lawrence.com/static/"
-#
-# To collect static files here, first ensure that your static files are
-# in apps' "static/" subdirectories and in STATICFILES_DIRS. Then use the
-# collectstatic management command.
-# Don't put anything in this directory manually.
-#
-# Then, use your web server's settings to serve STATIC_ROOT at the STATIC_URL.
-# This is done outside of Django, but the docs have some implementation
-# suggestions.
-# https://docs.djangoproject.com/en/dev/howto/static-files/deployment/
-#
-# This only applies for DEBUG = False. When DEBUG = True, static files
-# are served automagically with django.contrib.staticfiles.views.serve().
-STATIC_ROOT = SITE_DIR.child('static_serve')
-
-# The maximum size (in bytes) that an upload will be before it
-# gets streamed to the file system.
-FILE_UPLOAD_MAX_MEMORY_SIZE = 52428800  # 50 MB
-
-WSGI_APPLICATION = 'config.wsgi.application'
-
-
-
-# [Custom setting]
-# Verbosity of print messages printed by our unit tests' code. Note that
-# this is different from Django's test runner's verbosity setting, which
-# relates to messages printed by Django's test runner code.
-#
-# 0 means the unit tests won't print any additional messages.
-#
-# 1 means the unit tests will print additional messages as extra confirmation
-# that things worked.
-#
-# There is no 2 for now, unless we see a need for it later.
-UNIT_TEST_VERBOSITY = 0
-
-
-
-# VISION BACKEND SETTINGS
-# TODO: move to separate settings file.
-SLEEP_TIME_BETWEEN_IMAGE_PROCESSING = 60 * 60
+# Subject-line prefix for email messages sent with
+# django.core.mail.mail_admins or django.core.mail.mail_managers.
+# You'll probably want to include the trailing space.
+EMAIL_SUBJECT_PREFIX = '[CoralNet staging] '

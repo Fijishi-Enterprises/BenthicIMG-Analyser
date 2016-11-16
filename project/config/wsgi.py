6ea225d825eee7b5ee25a9c8bbf000b4131c90eb
StephenChan <StephenChan@users.noreply.github.com>
2016-11-15 18:49:08 -0800

    Remove user and group options from gunicorn config. Setting these requires running the gunicorn master process with sudo, and we don't currently know how to make sudo and virtualenv play nice. Just doesn't seem worth the effort yet. Change a couple of comments and strings.

--- project/config/wsgi.py
+++ project/config/wsgi.py
@@ -12,11 +12,12 @@ import os
 from django.core.exceptions import ImproperlyConfigured
 from django.core.wsgi import get_wsgi_application
 
-# One option is to set a default settings module here, but we don't really
+# We could set a default settings module here, but we don't really
 # have a one-size-fits-all settings module. So to avoid potential confusion,
 # we'll just ensure the settings module can only come from an env-var.
 if not os.environ.get('DJANGO_SETTINGS_MODULE'):
     raise ImproperlyConfigured(
-        "Must set the DJANGO_SETTINGS_MODULE environment variable.")
+        "Must set the DJANGO_SETTINGS_MODULE environment variable."
+        " Example value: config.settings.staging")
 
 application = get_wsgi_application()

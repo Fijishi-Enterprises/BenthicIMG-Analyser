1934bbdc285c0152dc00b40533590c2b605566f3
StephenChan <StephenChan@users.noreply.github.com>
2016-11-19 16:20:15 -0800

    URL cleanup. (1) Avoid URLs with redundant words such as labels/labels. (2) Remove obsolete label-import view from images app. (3) Remove extra backend URL include (the one with source id worked, the one without didn't work; kept the former).

--- project/images/urls.py
+++ project/images/urls.py
@@ -1,22 +1,27 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from . import views
 
+source_general_urlpatterns = [
+    url(r'^$', views.source_list, name="source_list"),
+    url(r'^about/$', views.source_about, name="source_about"),
+    url(r'^new/$', views.source_new, name="source_new"),
+    url(r'^invites/$', views.invites_manage, name="invites_manage"),
+]
+
+source_specific_urlpatterns = [
+    url(r'^$', views.source_main, name="source_main"),
+    url(r'^edit/$', views.source_edit, name="source_edit"),
+    url(r'^admin/$', views.source_admin, name="source_admin"),
+    url(r'^detail_box/$', views.source_detail_box, name="source_detail_box"),
+]
+
+image_urlpatterns = [
+    url(r'^view/$', views.image_detail, name="image_detail"),
+    url(r'^edit/$', views.image_detail_edit, name="image_detail_edit"),
+]
+
 urlpatterns = [
-    url(r'^source/$', views.source_list, name="source_list"),
-    url(r'^source/about/$', views.source_about, name="source_about"),
-    url(r'^source/new/$', views.source_new, name="source_new"),
-    url(r'^source/(?P<source_id>\d+)/$', views.source_main, name="source_main"),
-    url(r'^source/(?P<source_id>\d+)/edit/$', views.source_edit, name="source_edit"),
-    url(r'^source/(?P<source_id>\d+)/admin/$', views.source_admin, name="source_admin"),
-    url(r'^source/(?P<source_id>\d+)/detail_box/$', views.source_detail_box, name="source_detail_box"),
-
-    url(r'^image/(?P<image_id>\d+)/view/$', views.image_detail, name="image_detail"),
-    url(r'^image/(?P<image_id>\d+)/edit/$', views.image_detail_edit, name="image_detail_edit"),
-
-    # Consider moving this to annotations or upload, or else remove this if we're no longer using it.
-    url(r'^source/(?P<source_id>\d+)/label_import/$', views.import_labels, name="label_import"),
-
-    # Consider moving this into accounts, or messages, or a separate
-    # 'sources' app if we decide to have such a thing.
-    url(r'^invites_manage/$', views.invites_manage, name="invites_manage"),
+    url(r'^source/', include(source_general_urlpatterns)),
+    url(r'^source/(?P<source_id>\d+)/', include(source_specific_urlpatterns)),
+    url(r'^image/(?P<image_id>\d+)/', include(image_urlpatterns)),
 ]

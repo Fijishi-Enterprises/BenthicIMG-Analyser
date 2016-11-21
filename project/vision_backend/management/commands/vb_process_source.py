81f20c69c2debb90bc539f9987cab1ea09f942b4
Oscar Beijbom <oscar.beijbom@gmail.com>
2016-11-20 22:50:21 -0800

    added option to process a fixed nbr images from each source

--- project/vision_backend/management/commands/vb_process_source.py
+++ project/vision_backend/management/commands/vb_process_source.py
@@ -8,12 +8,31 @@ class Command(BaseCommand):
     help = 'Process source completely.'
 
     def add_arguments(self, parser):
-        parser.add_argument('source_ids', type=int, nargs='+', help="list of source ids to process")
+
+        parser.add_argument('mode', choices=['individual', 'parallel']) 
+        parser.add_argument('--source_ids', type=int, nargs='+', help="list of source ids to process")
+        parser.add_argument('--nbr_images', type=int, nargs=1, help="number of images per source")
 
     def handle(self, *args, **options):
-        for source_id in options['source_ids']:
-            source = Source.objects.get(id = source_id)
-            images = Image.objects.filter(source = source, features__extracted=False)
-            print "Submitting {} jobs for {}... ".format(images.count(), source.name)
-            for image in images:
-                submit_features.delay(image.id)
+
+        if options['mode'] == 'individual':
+            assert options['source_ids']
+            
+            print "Running in individual mode"
+            for source_id in options['source_ids']:
+                source = Source.objects.get(id = source_id)
+                images = Image.objects.filter(source = source, features__extracted=False)
+                print "Submitting {} jobs for {}... ".format(images.count(), source.name)
+                for image in images:
+                    submit_features.delay(image.id)
+
+        elif options['mode'] == 'parallel':
+            assert options['nbr_images']
+            nbr_images = options['nbr_images'][0]
+            
+            print "Running in parallel mode with {} images".format(nbr_images)
+            for source in Source.objects.filter():
+                images = Image.objects.filter(source = source, features__extracted=False)[:nbr_images]
+                print "Submitting {} jobs for {}... ".format(images.count(), source.name)
+                for image in images:
+                    submit_features.delay(image.id)

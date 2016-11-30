2e28ba1bfeb1fe5bd8e03094c13a3f2f2260b489
Oscar Beijbom <oscar.beijbom@gmail.com>
2016-11-29 21:06:09 -0800

    updated command syntax

--- project/images/management/commands/removepointoutliers.py
+++ project/images/management/commands/removepointoutliers.py
@@ -10,30 +10,40 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
 
-        parser.add_argument('dryrun', type=int, default = 1, nargs = '?', help="dryrun")
-        parser.add_argument('outfile', type=str)
+        parser.add_argument('mode', choices=['scan', 'print', 'clean'], help = "") 
+        parser.add_argument('file', type=str)
 
     def handle(self, *args, **options):
 
-    	dryrun = options['dryrun']
 
-        db = dict()
+        if options['mode'] == 'scan':       
+            db = dict()
 
-        for image in Image.objects.filter():
-        	for point in Point.objects.filter(image=image):
-        		if ((point.row > image.original_height) or (point.column > image.original_width)):
-        			if not image.pk in db.keys():
-        				db[image.pk] = []
-        			db[image.pk].append(point.pk)
+            for image in Image.objects.filter():
+                for point in Point.objects.filter(image=image):
+                    if ((point.row > image.original_height) or (point.column > image.original_width)):
+                        if not image.pk in db.keys():
+                            db[image.pk] = []
+                        db[image.pk].append(point.pk)
 
-        f = open(options['outfile'], 'w')
-        f.write(json.dumps(db))
-        
-        if dryrun:
-        	for image_pk in db.keys():
-        		image = Image.objects.get(pk = image_pk)
-        		print '--' + image.name + '--'
-        		for point_pk in db[image_pk]:
-        			point = Point.objecs.get(pk = point_pk)
-        			print point.row, point.column
+            f = open(options['file'], 'w')
+            f.write(json.dumps(db))
+
+        if options['mode'] == 'print':
+            f = open(options['file'], 'r')
+            db = json.loads(f.read())
+
+            for image_pk in db.keys():
+                print image_pk, len(db[image_pk])
+
+        if options['mode'] == 'clean':
+            f = open(options['file'], 'r')
+            db = json.loads(f.read())
+
+            for image_pk in db.keys():
+                for point_pk in db[image_pk]:
+                    point = Point.objects.get(pk = point_pk)
+                    point.delete()
+
+
 

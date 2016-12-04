7075772f524ead6a025d1fa94fabe0b04e90ab3c
Oscar Beijbom <oscar.beijbom@gmail.com>
2016-12-03 23:52:10 -0800

    new form for confustion matrix test page

--- project/vision_backend/forms.py
+++ project/vision_backend/forms.py
@@ -5,6 +5,9 @@ class TreshForm(forms.Form):
     confidence_threshold = forms.IntegerField(min_value=0, max_value=100)    
     labelmode = forms.ChoiceField((('full', 'Labels'), ('func', 'Functional Groups')))
         
+class CmTestForm(forms.Form):
+    nlabels = forms.IntegerField(min_value=0, max_value=200)    
+    namelength = forms.IntegerField(min_value=10, max_value=100)
 
 
 

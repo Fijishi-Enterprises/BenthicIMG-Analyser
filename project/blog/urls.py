2517b237ecfcaabc997070a4297c5906b5990625
StephenChan <StephenChan@users.noreply.github.com>
2019-08-04 21:40:01 -0700

    There's not really a point to keeping the `andablog` name on our blog files/dirs. Just rename them to blog.

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     # We've customized 2 out of 2 andablog views, so we don't need to
     # `include` the andablog views on top of this.
     url(r'^$',
-        views.EntriesList.as_view(),
+        views.EntriesList.as_view(
+            template_name='blog/entry_list.html'),
         name='entry_list'),
     url(r'^(?P<slug>[A-Za-z0-9-_]+)/$',
-        views.EntryDetail.as_view(),
+        views.EntryDetail.as_view(
+            template_name='blog/entry_detail.html'),
         name='entry_detail'),
 ]

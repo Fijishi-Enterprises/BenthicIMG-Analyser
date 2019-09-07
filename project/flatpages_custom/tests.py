8a1dc015eca3b34036d551652817417ae3823077
StephenChan <StephenChan@users.noreply.github.com>
2019-09-07 02:18:55 -0700

    Assume the flatpage content field is Markdown, and render the Markdown to HTML when displaying the flatpage.

--- project/flatpages_custom/tests.py
+++ project/flatpages_custom/tests.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from bs4 import BeautifulSoup
 from django.conf import settings
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.sites.models import Site
@@ -31,3 +32,24 @@ class FlatpagesTest(ClientTest):
         Should 404."""
         response = self.client.get('/pages/nonexistent-page/')
         self.assertTemplateUsed(response, '404.html')
+
+    def test_markdown_to_html(self):
+        """Markdown in the content field should be rendered as HTML when
+        displaying the flatpage."""
+        page = FlatPage(
+            url='/help/faq/', title="FAQ",
+            content="**FAQ contents** go *here.*")
+        page.save()
+        page.sites.add(Site.objects.get(pk=settings.SITE_ID))
+        page.save()
+
+        response = self.client.get('/pages/help/faq/')
+        response_soup = BeautifulSoup(response.content, 'html.parser')
+        container_soup = response_soup.find(
+            'div', dict(id='content-container'))
+
+        self.assertHTMLEqual(
+            str(container_soup),
+            '<div id="content-container">'
+            '<p><strong>FAQ contents</strong> go <em>here.</em></p>'
+            '</div>')

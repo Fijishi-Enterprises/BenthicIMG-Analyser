4b484becaf42f444da449cbc060ac5916e35738e
StephenChan <stephenjchan@gmail.com>
2020-02-11 20:58:03 -0800

    Change the deploy status/result URL patterns to indicate what the internal models actually are.

--- project/vision_backend_api/urls.py
+++ project/vision_backend_api/urls.py
@@ -8,8 +8,8 @@ from . import views
 urlpatterns = [
     url(r'^classifier/(?P<classifier_id>\d+)/deploy/$',
         views.Deploy.as_view(), name='deploy'),
-    url(r'^deploy_status/(?P<job_id>\d+)/$',
+    url(r'^deploy_job/(?P<job_id>\d+)/status/$',
         views.DeployStatus.as_view(), name='deploy_status'),
-    url(r'^deploy_result/(?P<job_id>\d+)/$',
+    url(r'^deploy_job/(?P<job_id>\d+)/result/$',
         views.DeployResult.as_view(), name='deploy_result'),
 ]

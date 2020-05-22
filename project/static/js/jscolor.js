ed5d4d978186d14b8370f4e189ddf2186eb9f957
StephenChan <stephenjchan@gmail.com>
2020-05-22 02:25:10 -0700

    Make a hack to jscolor.js to not use arguments.callee, thus keeping PyCharm from marking it as an errored file. Those old IE versions wouldn't load the rest of the annotation tool anyway (particularly the canvas).

--- project/static/js/jscolor.js
+++ project/static/js/jscolor.js
@@ -170,31 +170,13 @@ var jsc = {
 			// Fallback
 			window.addEventListener('load', fireOnce, false);
 
-		} else if (document.attachEvent) {
-			// IE
-			document.attachEvent('onreadystatechange', function () {
-				if (document.readyState === 'complete') {
-					document.detachEvent('onreadystatechange', arguments.callee);
-					fireOnce();
-				}
-			})
-
-			// Fallback
-			window.attachEvent('onload', fireOnce);
-
-			// IE7/8
-			if (document.documentElement.doScroll && window == window.top) {
-				var tryScroll = function () {
-					if (!document.body) { return; }
-					try {
-						document.documentElement.doScroll('left');
-						fireOnce();
-					} catch (e) {
-						setTimeout(tryScroll, 1);
-					}
-				};
-				tryScroll();
-			}
+		} else {
+			// HACK for CoralNet. The original code had an older-IE shim which
+			// used arguments.callee, a language feature which is disallowed
+			// in ES5 strict mode. This made PyCharm inspections draw ugly red
+			// underlines on our 'static' directory.
+			console.log(
+				"Browser doesn't support addEventListener; things may break.");
 		}
 	},
 

35629c6151a1838c7bda07592708ec51d27b9781
Oscar Beijbom <oscar.beijbom@gmail.com>
2020-06-06 00:01:57 -0700

    tried another fix for spacer aws permissions

--- project/config/settings/dev_beijbom.py
+++ project/config/settings/dev_beijbom.py
@@ -3,5 +3,6 @@ from .base_devserver import *
 ALLOWED_HOSTS = ['127.0.0.1', 'testserver']
 # Pick one.
 from .storage_s3 import *
-from .storage_local import *
+VISION_BACKEND_CHOICE = 'vision_backend.queues.SQSQueue'
+# from .storage_local import *
 # from .storage_s3_regtests import *

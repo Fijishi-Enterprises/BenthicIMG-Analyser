0bc88e1d9e36072f95923d38412f5fe618f88a6e
Oscar Beijbom <oscar.beijbom@gmail.com>
2020-06-07 11:15:51 -0700

    removed the regtests configs, updated pyspacer req

--- project/config/settings/dev_beijbom.py
+++ project/config/settings/dev_beijbom.py
@@ -2,8 +2,7 @@ from .base_devserver import *
 
 ALLOWED_HOSTS = ['127.0.0.1', 'testserver']
 
-# VISION_BACKEND_CHOICE = 'vision_backend.queues.SQSQueue'
+# SPACER_QUEUE_CHOICE = 'vision_backend.queues.SQSQueue'
+# from .storage_s3 import *
+from .storage_local import *
 
-from .storage_s3 import *
-# from .storage_local import *
-# from .storage_s3_regtests import *

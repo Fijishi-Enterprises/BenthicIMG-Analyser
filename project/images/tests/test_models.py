df5674181a7299b561794cb9ca51662aebfebc81
StephenChan <stephenjchan@gmail.com>
2020-10-02 02:21:06 -0700

    Write test for the last_annotation data migration. Remove the previous migration test in this file, since it's no longer needed and no longer works (perhaps MigrationTest is picky when a migration other than the latest migration is being tested?).

--- project/images/tests/test_models.py
+++ project/images/tests/test_models.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 from io import BytesIO
-import mock
 
 from django.core.files.base import ContentFile
 from django_migration_testcase import MigrationTest
@@ -8,7 +7,6 @@ from easy_thumbnails.files import get_thumbnailer
 import piexif
 from PIL import Image as PILImage
 
-from annotations.model_utils import AnnotationAreaUtils
 from images.models import Point
 from images.model_utils import PointGen
 from lib.tests.utils import ClientTest
@@ -121,65 +119,69 @@ class PointValidationTest(ClientTest):
             point.save()
 
 
-def save_without_checks(self, *args, **kwargs):
-    """
-    Mock version of Point.save().
-    Doesn't run assertions, so we can save any row/column values we want.
-    """
-    super(Point, self).save(*args, **kwargs)
-
-
-class PointRowcolIndexingMigrationTest(MigrationTest, ClientTest):
+class PopulateLastAnnotationMigrationTest(MigrationTest, ClientTest):
 
     app_name = 'images'
-    before = '0022'
-    after = '0023'
+    before = '0024'
+    after = '0025'
 
     def test_migration(self):
         user = self.create_user()
         source = self.create_source(
             user,
-            min_x=0,
-            max_x=100,
-            min_y=0,
-            max_y=100,
             point_generation_type=PointGen.Types.SIMPLE,
-            simple_number_of_points=1)
+            simple_number_of_points=2)
+        labels = self.create_labels(user, ['A', 'B'], 'GroupA')
+        self.create_labelset(user, source, labels)
 
-        img1 = self.upload_image(
-            user, source, image_options=dict(width=50, height=50))
-        img2 = self.upload_image(
-            user, source, image_options=dict(width=50, height=50))
+        # No annotations.
+        img1 = self.upload_image(user, source)
 
-        # Create points manually and without checks so we can set the
-        # rows/columns we want.
-        Point.objects.all().delete()
-        with mock.patch.object(Point, 'save', save_without_checks):
-            point = Point(image=img1, point_number=1, row=1, column=50)
-            point.save()
-            point = Point(image=img2, point_number=1, row=29, column=1)
-            point.save()
+        # One annotation.
+        img2 = self.upload_image(user, source)
+        self.add_annotations(user, img2, {1: 'A'})
 
-        # Create a pixel-based annotation area for only one image. Leave the
-        # other as a percent-based area.
-        img2.metadata.annotation_area = \
-            AnnotationAreaUtils.pixels_to_db_format(1, 15, 1, 45)
-        img2.metadata.save()
+        # Two annotations, latest on point 1.
+        img3 = self.upload_image(user, source)
+        self.add_annotations(user, img3, {2: 'B'})
+        self.add_annotations(user, img3, {1: 'A'})
 
-        # Convert from 1-indexing to 0-indexing.
+        # Two annotations, latest on point 2.
+        img4 = self.upload_image(user, source)
+        self.add_annotations(user, img4, {1: 'A'})
+        self.add_annotations(user, img4, {2: 'B'})
+
+        # Reset last_annotation fields.
+        img1.last_annotation = None
+        img1.save()
+        img2.last_annotation = None
+        img2.save()
+        img3.last_annotation = None
+        img3.save()
+        img4.last_annotation = None
+        img4.save()
+
+        # Set last_annotation fields with the migration.
         self.run_migration()
 
-        # Check that Points have been converted.
-        point = Point.objects.get(image=img1)
-        self.assertEqual(point.row, 0)
-        self.assertEqual(point.column, 49)
-        point = Point.objects.get(image=img2)
-        self.assertEqual(point.row, 28)
-        self.assertEqual(point.column, 0)
+        # Check last_annotation values.
+        img1.refresh_from_db()
+        self.assertIsNone(img1.last_annotation)
 
-        # Check that pixel-based annotation areas have been converted,
-        # and that percent-based ones stay the same.
-        img1.metadata.refresh_from_db()
-        self.assertEqual(img1.metadata.annotation_area, '0;100;0;100')
-        img2.metadata.refresh_from_db()
-        self.assertEqual(img2.metadata.annotation_area, '0,14,0,44')
+        img2.refresh_from_db()
+        self.assertIsNotNone(img2.last_annotation)
+        self.assertEqual(
+            img2.last_annotation.pk,
+            img2.annotation_set.get(point__point_number=1).pk)
+
+        img3.refresh_from_db()
+        self.assertIsNotNone(img3.last_annotation)
+        self.assertEqual(
+            img3.last_annotation.pk,
+            img3.annotation_set.get(point__point_number=1).pk)
+
+        img4.refresh_from_db()
+        self.assertIsNotNone(img4.last_annotation)
+        self.assertEqual(
+            img4.last_annotation.pk,
+            img4.annotation_set.get(point__point_number=2).pk)

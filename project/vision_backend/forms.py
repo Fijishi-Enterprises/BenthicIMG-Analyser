cd6e7f53f59b650d151f8308c256566d96afc1d2
StephenChan <stephenjchan@gmail.com>
2021-04-19 21:09:28 -0700

    In vision_backend's TreshForm, use a kwarg for the ChoiceField since Django 2.0 requires that.

--- project/vision_backend/forms.py
+++ project/vision_backend/forms.py
@@ -1,13 +1,12 @@
-from labels.models import LocalLabel
 from django import forms
+
 
 class TreshForm(forms.Form):
     confidence_threshold = forms.IntegerField(min_value=0, max_value=100)    
-    labelmode = forms.ChoiceField((('full', 'Labels'), ('func', 'Functional Groups')))
-        
+    labelmode = forms.ChoiceField(
+        choices=(('full', 'Labels'), ('func', 'Functional Groups')))
+
+
 class CmTestForm(forms.Form):
     nlabels = forms.IntegerField(min_value=0, max_value=200)    
     namelength = forms.IntegerField(min_value=10, max_value=100)
-
-
-

09913670ff5e8ab21f3e4aad98c31232f286834f
StephenChan <stephenjchan@gmail.com>
2021-06-09 15:04:28 -0700

    Simplify the test utility to create a default calcification rate table.

--- project/calcification/tests/utils.py
+++ project/calcification/tests/utils.py
@@ -1,16 +1,13 @@
 from ..models import CalcifyRateTable
 
 
-class CalcifyTestMixin:
-
-    @staticmethod
-    def create_global_rate_table(region, rates_dict):
-        table = CalcifyRateTable(
-            name="Table Name - {}".format(region),
-            description="Table Description",
-            rates_json=rates_dict,
-            source=None,
-            region=region,
-        )
-        table.save()
-        return table
+def create_default_calcify_table(region, rates_dict):
+    table = CalcifyRateTable(
+        name="Table Name - {}".format(region),
+        description="Table Description",
+        rates_json=rates_dict,
+        source=None,
+        region=region,
+    )
+    table.save()
+    return table
